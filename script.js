35b0d091457ed0cfab9e54928196681b17b50a44
Beast <beastcode9999@gmail.com>
2022-08-28 18:42:45 +0900

    add regex for email and password

--- script.js
+++ script.js
@@ -23,34 +23,44 @@ let userValid = false;
 
 // regex to check validity
 const usernameRegex = /.{4,}/;
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+const passwordRegex = /.{6,}/; 
 
 
 
+// console.log(usernameRegex.test(username.value))
 
-username.addEventListener('click', (e)=> {
-  
-console.log(usernameRegex.test(username.value))
-
-    if(usernameRegex.test(username.value)) {
-        console.log(username.value)
-        userValid = true;
+window.addEventListener('click', (e)=> {
+    // console.log(e.target.id)
+    if (username === document.activeElement) {
+        
+    } else{ 
+        if(usernameRegex.test(username.value)) {
+            // console.log(username.value)
+            userValid = true;
+        }
+        if(!usernameRegex.test(username.value)) {
+            // console.log(username.value)
+            userValid = false;
+        }
     }
-    if(!usernameRegex.test(username.value)) {
-        console.log(username.value)
+    // console.log(userValid)
+    activeSection();
 
-        userValid = false;
-    }
+})
 
-    console.log(userValid)
+function activeSection() {
     formSection.forEach((section) => {
         if(userValid === false) {
-            // section.classList.toggle("")
+            if((section.dataset.inputFor === "username")) {
+                section.classList.add('invalid')
+            }
+            else {
+            }
         }
         
     })
-})
-
-// console.log(userValid)
+}
 
 submit.addEventListener('click', submitForm);
 
