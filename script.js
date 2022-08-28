8fc3bd38715717d213ff8d41ed17a210084ef174
Beast Code <beastcode9999@gmail.com>
2022-08-28 15:58:12 +0900

    add selectors on script.js for input elements

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const darkMode = document.querySelector(".header__item--special--dark");
 const lightMode = document.querySelector(".header__item--special--light");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const phoneNumber = document.getElementById("phone-number");
+const password = document.getElementById("user_password");
+const passwordConfirm = document.getElementById("user_password_confirmation");
+const footerInput = document.getElementById("footer__input");
 
-console.log(darkMode)
-console.log(lightMode)
+footerInput.style.borderBlockColor = "green"
+console.log(footerInput)
+
+
