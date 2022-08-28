00df3e739edbef312118bf9252874918b799d06e
Beast Code <beastcode9999@gmail.com>
2022-08-28 15:16:26 +0900

    add fix for hover css on header links

---
+++ script.js
@@ -0,0 +1,5 @@
+const darkMode = document.querySelector(".header__item--special--dark");
+const lightMode = document.querySelector(".header__item--special--light");
+
+console.log(darkMode)
+console.log(lightMode)
