dbe4b78c7b294170fea4ed21ef7d9a6a4e3eb833
Beast Code <beastcode9999@gmail.com>
2022-08-30 01:50:03 +0900

    changed phone number regex

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const phoneContainer = document.getElementById("number-container");
 const passwordContainer = document.getElementById("password-container");
 const passwordConfirmationContainer = document.getElementById("password_confirmation-container");
 
-console.log(phoneContainer)
 const footerInput = document.getElementById("footer__input");
 const submit = document.getElementById("submit");
 
@@ -117,6 +116,8 @@ window.addEventListener("click", () => {
     addErrorMessage()
 })
 
+
+
 // insertAfter(createErrorMessage("username", "hello world"), emailContainer)
 
 // create a function that creates an error message when called
@@ -144,7 +145,7 @@ function insertAfter(el, referenceNode) {
 const usernameRegex = /.{4,}/;
 const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 const passwordRegex = /.{6,}/; 
-const phoneNumberRegex = /\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*(\d{1,2})$/;
+const phoneNumberRegex = /^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
 
 // console.log(usernameRegex.test(username.value))
 
