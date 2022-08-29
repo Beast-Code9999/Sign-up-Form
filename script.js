f75b212e0dfc453e18e0a63b93eaaa35abd231a7
Beast Code <beastcode9999@gmail.com>
2022-08-29 14:51:28 +0900

    add regex for phone numbers

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const passwordConfirmationContainer = document.getElementById("password_confirma
 // const passwordConfirmation = document.getElementById("user_password_confirmation");
 const input = document.querySelectorAll("input")
 const footerInput = document.getElementById("footer__input");
-const submit = document.getElementById("submit")
+const submit = document.getElementById("submit");
 
 window.addEventListener('click', activeElement)
 
@@ -20,6 +20,7 @@ function activeElement() {
     input.forEach((input) => {
         checkValidity(input, "username", usernameRegex);
         checkValidity(input, "email", emailRegex);
+        checkValidity(input, "phone-number", phoneNumberRegex)
         checkValidity(input, "user_password", passwordRegex)
     })
 }
@@ -55,7 +56,7 @@ let userValid = false;
 const usernameRegex = /.{4,}/;
 const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 const passwordRegex = /.{6,}/; 
-
+const phoneNumberRegex = /\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*(\d{1,2})$/
 
 
 // console.log(usernameRegex.test(username.value))
